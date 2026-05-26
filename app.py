2c3d830ff91e58938928ad140846b9d7165579d6
cauanicolas <nicolascaua57@gmail.com>
2026-05-26 12:34:28 -0300

    feat: adiciona integrantes

--- app.py
+++ app.py
@@ -84,6 +84,25 @@ styles = {
     },
 }
 
+st.markdown("""
+<style>
+div.stButton > button {
+    width: 100%;
+    height: 100px;
+    font-size: 18px;
+    border-radius: 12px;
+    background-color: #2C3A6B;
+    color: white;
+}
+div.stButton > button:hover {
+    background-color: #F2C94C;
+    color: #2C3A6B;
+    transform: translateY(-1px);
+    letter-spacing: .5px;
+}
+</style>
+""", unsafe_allow_html=True)
+
 # Cria a navbar
 try:
     page = st_navbar(
@@ -101,12 +120,66 @@ except TypeError:
         styles=styles,
         css=navbar_css,
     )
+
+col3, col4, col5= st.columns([2,1,2])
+with col4:
+    st.image("./assets/logo.png", width=300)
     
 st.title("PET-Saúde")
 st.subheader("GT 6: Vigilância Alimentar e Nutricional")
 st.write("O GT 6 foca na capacitação de profissionais da Atenção Básica para o uso adequado de marcadores alimentares no SUS, integrando a transformação digital à saúde pública")
+st.divider()
+# BOTÕES CENTRALIZADOS
+col1, col2, col3, col4 = st.columns([2,1,1,2])
+
+with col2:
+    btn1 = st.button("Submeter Dados")
+
+with col3:
+    btn2 = st.button("Sala de Situação Alimentar")
+
+# AÇÕES
+if btn1:
+    st.write("Dashboard clicado")
+
+if btn2:
+    st.write("Relatórios clicado")
+
+st.divider()
+
+st.title("Equipe GT-6")
+st.header("Integrantes")
+st.subheader("Gestão")
+st.write("**Patrícia de Fragas** - Coordenadora")
+st.write("**David Francisco** - Coordenador Adjunto")
+st.write("**Rafaela Garcia** - Gestora Júnior")
+st.write("**Raynara Ferreira** - Gestor Júnior")
+
+st.subheader("Equipe de Comunicação")
+st.write("**Eduarda Nascimento** - Comunicação e Design")
+st.write("**Yasmin Fagundes** - Comunicação e Design")
+
+st.subheader("Equipe de TI")
+st.write("**Cauã Nicolas** - TI")
+st.write("**Pedro Augusto** - TI")
+st.write("**Jean Carlos** - TI")
+
+st.subheader("Equipe de Pesquisa")
+st.write("**Débora Beatriz** - Pesquisa")
+st.write("**Kelly Cristina** - Pesquisa")    
+st.write("**Taís Nazário** - Pesquisa")
+st.write("**Tainá Ribeiro** - Pesquisa")
+st.write("**Talitha de Souza** - Pesquisa")
+
+st.subheader("Preceptores")
+st.write("**Ana Paula Loschi** - Preceptora - UBS Jardins Mangueiral")
+st.write("**Leniela Afra** - Preceptora - DIRAPS")
+st.write("**Vanessa Araújo** - Preceptora - UBS Gama")
+
+st.divider()
 
 st.write(f"Página atual: {page}")
+
 col1, col2 = st.columns(2)
 with col1:
     st.image("./assets/logounb.jpg", width=80)
